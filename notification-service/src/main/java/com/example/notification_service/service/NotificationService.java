76669eff175b8075c570e77097f7b1acb243269a
DonaldKisaka <davidkisaka4@gmail.com>
2026-02-06 21:35:40 +0300

    Final commit

--- notification-service/src/main/java/com/example/notification_service/service/NotificationService.java
+++ notification-service/src/main/java/com/example/notification_service/service/NotificationService.java
@@ -1,5 +1,6 @@
 package com.example.notification_service.service;
 
+import com.example.notification_service.event.OrderNotification;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
@@ -11,6 +12,14 @@ public class NotificationService {
     @KafkaListener(topics = "order-placed", groupId = "notification-service")
     public void getOrderNumber(String orderId) {
         log.info("Received order id: {}", orderId);
+    }
 
+
+    @KafkaListener(topics = "order-placed-email", groupId = "notification-service")
+    public void getOrderNotification(OrderNotification orderNotification) {
+        log.info("Received order id: {}", orderNotification.getOrderId());
+
+        // Send notification
+        log.info("Notification Sent: {}", orderNotification.toString());
     }
 }
